a8668828c90a8f65a1ea3ff0ecbf4b66813eb54f
Sayan Samanta <samantasayan23@gmail.com>
2022-02-01 17:55:11 +0530

    details page started

--- src/components/Details.js
+++ src/components/Details.js
@@ -9,21 +9,34 @@ function Details() {
     const {all} = useContext(GlobalContext);
     const [data, setData] = all;
     const [orgData, setOrgData] = useState({});
+    const [allTech, setAllTech] = useState([]);
+    const [allTopic, setAllTopic] = useState([]);
+
     useEffect(() => {
         data.forEach((el) => {
-            if (el.slug == org) {
+            if (el.slug === org) {
                 setOrgData(el);
             }
         });
-        console.log(orgData.logo_bg_color);
+        // console.log(typeof orgData.tech_tags);
     });
+    useEffect(() => {
+        setAllTech([orgData.tech_tags]);
+    }, [orgData]);
 
     return (
     <div>
         <Nav />
         <div className='container'>
-            <img className={`org-detail-img ${orgData.logo_bg_color == "white" ? "white" : ""}`} src={orgData.logo_url} />
+            <img className={`org-detail-img ${orgData.logo_bg_color === "white" ? "white" : ""}`} src={orgData.logo_url} />
             <h2>{orgData.name}</h2>
+            <p className='tech-details'>
+                {allTech.map((el, i) => {
+                    return(
+                        <span className='each-tech' key={i}>{el}</span>
+                    )
+                })}
+            </p>
         </div>
     </div>
     );
